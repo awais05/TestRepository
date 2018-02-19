00e6ed2288ea29cb27e301ad40e954769fc39c9b
First Last <@amazon.com>
2018-02-19 16:38:54 +0530

    Changed = to :

--- ForLoop.java
+++ ForLoop.java
@@ -2,7 +2,7 @@ public class ForLoop{
 	public static void main(String args[]){
 		int n = 1;
 		for (int i=0;i<5;i++){
-			System.out.println("i = " + i);
+			System.out.println("i : " + i);
 		}
 	}
 }
